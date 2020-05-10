959366f088afd177dd6e2bcd25fe80e8e7b2c2f1
Surender Singh <surender.s@flipkart.com>
2020-05-10 15:12:19 +0530

    Integrated User Service

---
+++ cartManager/cartServer/src/main/java/com/example/cart/service/server/external/service/UserService.java
@@ -0,0 +1,34 @@
+package com.example.cart.service.server.external.service;
+
+import com.example.cart.service.common.datatypes.enums.Gender;
+import com.example.user.manager.datatypes.CreateUserRequest;
+import com.example.user.manager.datatypes.UserResponse;
+import com.example.user.service.client.UserServiceClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserService {
+
+    @Autowired
+    private UserServiceClient userServiceClient;
+
+    public UserResponse getUser(String userId) {
+        UserResponse userResponse = userServiceClient.getUserById(userId);
+
+        return userResponse;
+    }
+
+    //This method will not be used in this service
+    public UserResponse createUser(String name, String userId, Long age, Gender gender) {
+        CreateUserRequest createUserRequest = new CreateUserRequest();
+
+        createUserRequest.setAge(age);
+        createUserRequest.setGender(gender);
+        createUserRequest.setName(name);
+        createUserRequest.setUserId(userId);
+
+        UserResponse userResponse = userServiceClient.createUser(createUserRequest);
+        return userResponse;
+    }
+}
--- cartManager/cartServer/src/main/java/com/example/cart/service/server/service/impl/CartServiceImpl.java
+++ cartManager/cartServer/src/main/java/com/example/cart/service/server/service/impl/CartServiceImpl.java
@@ -3,8 +3,11 @@ package com.example.cart.service.server.service.impl;
 import com.example.cart.service.datatypes.CartStatus;
 import com.example.cart.service.datatypes.CreateCartRequest;
 import com.example.cart.service.server.entity.Cart;
+import com.example.cart.service.server.external.service.UserService;
 import com.example.cart.service.server.repository.CartRepository;
 import com.example.cart.service.server.service.CartService;
+import com.example.user.manager.datatypes.UserResponse;
+import com.example.user.manager.datatypes.UserState;
 import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -14,6 +17,8 @@ public class CartServiceImpl implements CartService {
     @Autowired
     private CartRepository cartRepository;
 
+    @Autowired
+    private UserService userService;
 
     @Override
     public Cart getCartById(Long cartId) {
@@ -22,7 +27,9 @@ public class CartServiceImpl implements CartService {
     }
 
     @Override
-    public Cart createCart(CreateCartRequest createCartRequest) {
+    public Cart createCart(CreateCartRequest createCartRequest) throws Exception {
+
+        validateUser(createCartRequest.getUserId());
         Cart cart = new Cart();
 
         cart.setCartStatus(CartStatus.OPENED);
@@ -31,6 +38,15 @@ public class CartServiceImpl implements CartService {
         cart.setCreatedBy(createCartRequest.getUserId());
 
         return cartRepository.save(cart);
+    }
+
+    //Method validates that user should be in verified state.
+    private void validateUser(String userId) throws Exception {
+        UserResponse userResponse = userService.getUser(userId);
+
+        if (!userResponse.getUserState().equals(UserState.ACTIVE)) {
+            throw new Exception("User is not active");
+        }
     }
 
 }
